1dddc765156b31ee23a4b7bb2736a1ac668e2c66
Yap-TS <yap.tze.syn@student.mmu.edu.my>
2025-11-11 11:25:41 +0800

    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 New Name")
+st.title("🎈 Yap's website")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
