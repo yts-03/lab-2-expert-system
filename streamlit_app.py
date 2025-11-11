0400f7910f5e096dabaf6608d462eea79063b5bc
Yap-TS <yap.tze.syn@student.mmu.edu.my>
2025-11-11 11:26:40 +0800

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Yap's website")
+st.title("🎈 Lab demo Update title")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
